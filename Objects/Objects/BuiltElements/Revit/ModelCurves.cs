0c53eda61c62dc1d52d6485f30c6441daecd1f99
Jenessa Man <jenessa.man@arup.com>
2021-07-12 16:39:15 -0400

    Add space separation line object

--- Objects/Objects/BuiltElements/Revit/ModelCurves.cs
+++ Objects/Objects/BuiltElements/Revit/ModelCurves.cs
@@ -4,55 +4,71 @@ using System.Collections.Generic;
 
 namespace Objects.BuiltElements.Revit.Curve
 {
-  public class ModelCurve : Base
-  {
-    public ICurve baseCurve { get; set; }
-    public string lineStyle { get; set; }
-    public List<Parameter> parameters { get; set; }
-    public string elementId { get; set; }
-
-    public ModelCurve() { }
-
-    [SchemaInfo("ModelCurve", "Creates a Revit model curve", "Revit", "Curves")]
-    public ModelCurve([SchemaMainParam] ICurve baseCurve, string lineStyle, List<Parameter> parameters = null)
+    public class ModelCurve : Base
     {
-      this.baseCurve = baseCurve;
-      this.lineStyle = lineStyle;
-      this.parameters = parameters;
+        public ICurve baseCurve { get; set; }
+        public string lineStyle { get; set; }
+        public List<Parameter> parameters { get; set; }
+        public string elementId { get; set; }
+
+        public ModelCurve() { }
+
+        [SchemaInfo("ModelCurve", "Creates a Revit model curve", "Revit", "Curves")]
+        public ModelCurve([SchemaMainParam] ICurve baseCurve, string lineStyle, List<Parameter> parameters = null)
+        {
+            this.baseCurve = baseCurve;
+            this.lineStyle = lineStyle;
+            this.parameters = parameters;
+        }
     }
-  }
 
-  public class DetailCurve : Base
-  {
-    public ICurve baseCurve { get; set; }
-    public string lineStyle { get; set; }
-    public List<Parameter> parameters { get; set; }
-    public string elementId { get; set; }
-
-    public DetailCurve() { }
-
-    [SchemaInfo("DetailCurve", "Creates a Revit detail curve", "Revit", "Curves")]
-    public DetailCurve([SchemaMainParam] ICurve baseCurve, string lineStyle, List<Parameter> parameters = null)
+    public class DetailCurve : Base
     {
-      this.baseCurve = baseCurve;
-      this.lineStyle = lineStyle;
-      this.parameters = parameters;
+        public ICurve baseCurve { get; set; }
+        public string lineStyle { get; set; }
+        public List<Parameter> parameters { get; set; }
+        public string elementId { get; set; }
+
+        public DetailCurve() { }
+
+        [SchemaInfo("DetailCurve", "Creates a Revit detail curve", "Revit", "Curves")]
+        public DetailCurve([SchemaMainParam] ICurve baseCurve, string lineStyle, List<Parameter> parameters = null)
+        {
+            this.baseCurve = baseCurve;
+            this.lineStyle = lineStyle;
+            this.parameters = parameters;
+        }
     }
-  }
 
-  public class RoomBoundaryLine : Base
-  {
-    public ICurve baseCurve { get; set; }
-    public List<Parameter> parameters { get; set; }
-    public string elementId { get; set; }
-
-    public RoomBoundaryLine() { }
-
-    [SchemaInfo("RoomBoundaryLine", "Creates a Revit room boundary line", "Revit", "Curves")]
-    public RoomBoundaryLine([SchemaMainParam] ICurve baseCurve, List<Parameter> parameters = null)
+    public class RoomBoundaryLine : Base
     {
-      this.baseCurve = baseCurve;
-      this.parameters = parameters;
+        public ICurve baseCurve { get; set; }
+        public List<Parameter> parameters { get; set; }
+        public string elementId { get; set; }
+
+        public RoomBoundaryLine() { }
+
+        [SchemaInfo("RoomBoundaryLine", "Creates a Revit room boundary line", "Revit", "Curves")]
+        public RoomBoundaryLine([SchemaMainParam] ICurve baseCurve, List<Parameter> parameters = null)
+        {
+            this.baseCurve = baseCurve;
+            this.parameters = parameters;
+        }
     }
-  }
+
+    public class SpaceSeparationLine : Base
+    {
+        public ICurve baseCurve { get; set; }
+        public List<Parameter> parameters { get; set; }
+        public string elementId { get; set; }
+
+        public SpaceSeparationLine() { }
+
+        [SchemaInfo("SpaceSeparationLine", "Creates a Revit space separation line", "Revit", "Curves")]
+        public SpaceSeparationLine([SchemaMainParam] ICurve baseCurve, List<Parameter> parameters = null)
+        {
+            this.baseCurve = baseCurve;
+            this.parameters = parameters;
+        }
+    }
 }
