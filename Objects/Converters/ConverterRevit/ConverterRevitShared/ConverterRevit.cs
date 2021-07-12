fa23428c227615d9295283fbf4fa985b258799f3
Jenessa Man <jenessa.man@arup.com>
2021-07-12 16:39:59 -0400

    Add conversion methods for spaces and space separation lines

--- Objects/Converters/ConverterRevit/ConverterRevitShared/ConverterRevit.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/ConverterRevit.cs
@@ -99,6 +99,9 @@ namespace Objects.Converter.Revit
           if ((BuiltInCategory)o.Category.Id.IntegerValue == BuiltInCategory.OST_RoomSeparationLines)
           {
             returnObject = RoomBoundaryLineToSpeckle(o);
+          } else if ((BuiltInCategory)o.Category.Id.IntegerValue == BuiltInCategory.OST_MEPSpaceSeparationLines)
+          {
+            returnObject = SpaceSeparationLineToSpeckle(o);
           }
           else
           {
@@ -126,6 +129,9 @@ namespace Objects.Converter.Revit
           break;
         case DB.Mechanical.Duct o:
           returnObject = DuctToSpeckle(o);
+          break;
+        case DB.Mechanical.Space o:
+          returnObject = SpaceToSpeckle(o);
           break;
         case DB.Plumbing.Pipe o:
           returnObject = PipeToSpeckle(o);
@@ -273,6 +279,9 @@ namespace Objects.Converter.Revit
         case BERC.RoomBoundaryLine o:
           return RoomBoundaryLineToNative(o);
 
+        case BERC.SpaceSeparationLine o:
+          return SpaceSeparationLineToNative(o);
+
         case BE.Roof o:
           return RoofToNative(o);
 
@@ -310,6 +319,9 @@ namespace Objects.Converter.Revit
         case BE.GridLine o:
           return GridLineToNative(o);
 
+        case BE.Space o:
+            return SpaceToNative(o);
+
         // other
         case Other.BlockInstance o:
           return BlockInstanceToNative(o);
@@ -342,6 +354,7 @@ namespace Objects.Converter.Revit
         DB.Architecture.TopographySurface _ => true,
         DB.Wall _ => true,
         DB.Mechanical.Duct _ => true,
+        DB.Mechanical.Space _ => true,
         DB.Plumbing.Pipe _ => true,
         DB.Electrical.Wire _ => true,
         DB.CurtainGridLine _ => true, //these should be handled by curtain walls
@@ -401,6 +414,7 @@ namespace Objects.Converter.Revit
         BE.View3D _ => true,
         BE.Room _ => true,
         BE.GridLine _ => true,
+        BE.Space _ => true,
         Other.BlockInstance _ => true,
         _ => false
 
